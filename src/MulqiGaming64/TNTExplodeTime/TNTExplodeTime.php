75d3232cdfbc330a26a233f57f5bb355d007137e
MulqiGaming64 <mulqi311@gmail.com>
2022-02-27 12:51:22 +0700

    Change BlockFactory to VanillaBlocks

--- src/MulqiGaming64/TNTExplodeTime/TNTExplodeTime.php
+++ src/MulqiGaming64/TNTExplodeTime/TNTExplodeTime.php
@@ -21,6 +21,7 @@ use pocketmine\utils\Random;
 use pocketmine\entity\projectile\Arrow;
 
 use pocketmine\block\TNT;
+use pocketmine\block\VanillaBlocks;
 use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\entity\ProjectileHitBlockEvent;
@@ -29,7 +30,6 @@ use pocketmine\item\Durable;
 use pocketmine\item\enchantment\VanillaEnchantments;
 use pocketmine\item\FlintSteel;
 use pocketmine\item\Item;
-use pocketmine\block\BlockFactory;
 
 use MulqiGaming64\TNTExplodeTime\TNT\TNTExplode;
 
@@ -101,7 +101,7 @@ class TNTExplodeTime extends PluginBase implements Listener{
     	$pos = $block->getPosition();
     	if($block instanceof TNT){
     		if($entity instanceof Arrow and $entity->isOnFire()){
-    			$pos->getWorld()->setBlock($pos, BlockFactory::getInstance()->get(0, 0));
+    			$pos->getWorld()->setBlock($pos, VanillaBlocks::AIR());
     			$location = Location::fromObject($pos->add(0.5, 0, 0.5), $pos->getWorld());
     			$this->spawnTNT($location, $this->getTime());
     		}
@@ -118,7 +118,7 @@ class TNTExplodeTime extends PluginBase implements Listener{
 				if($item instanceof Durable){
 					$item->applyDamage(1);
 				}
-				$pos->getWorld()->setBlock($pos, BlockFactory::getInstance()->get(0, 0));
+				$pos->getWorld()->setBlock($pos, VanillaBlocks::AIR());
 				$location = Location::fromObject($pos->add(0.5, 0, 0.5), $pos->getWorld());
     			$this->spawnTNT($location, $this->getTime());
     		}
